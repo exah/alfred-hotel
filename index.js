2e7a2a48b0175b30ff68a999038e6334a966da78
John Grishin <r@exah.ru>
2017-02-07 19:13:42 +0300

    Let Alfred filter list for us

--- index.js
+++ index.js
@@ -3,40 +3,38 @@ const { HOST = 'http://127.0.0.1:2000', TLD = 'dev' } = process.env
 
 alfy.fetch(`${HOST}/_/servers`)
   .then(servers => {
-    const list = alfy
-      .inputMatches(Object.keys(servers))
-      .map(id => {
-        const { status } = servers[id]
-        const url = `http://${id}.${TLD}`
-        const run = command => `{
-          "alfredworkflow": {
-            "arg": "${HOST}/_/servers/${id}",
-            "variables": {
-              "command": "${command}"
-            }
-          }
-        }`
-
-        return {
-          uid: url,
-          title: id,
-          subtitle: status,
-          arg: url,
-          quicklookurl: status === 'running' ? url : '',
-          mods: {
-            alt: {
-              arg: `${HOST}/${id}`,
-              subtitle: 'open without proxy'
-            },
-            cmd: status === 'running'
-              ? {
-                arg: run('stop'),
-                subtitle: 'stop'
-              }
-              : {}
+    const list = Object.keys(servers).map(id => {
+      const { status } = servers[id]
+      const url = `http://${id}.${TLD}`
+      const run = command => `{
+        "alfredworkflow": {
+          "arg": "${HOST}/_/servers/${id}",
+          "variables": {
+            "command": "${command}"
           }
         }
-      })
+      }`
+
+      return {
+        uid: url,
+        title: id,
+        subtitle: status,
+        arg: url,
+        quicklookurl: status === 'running' ? url : '',
+        mods: {
+          alt: {
+            arg: `${HOST}/${id}`,
+            subtitle: 'open without proxy'
+          },
+          cmd: status === 'running'
+            ? {
+              arg: run('stop'),
+              subtitle: 'stop'
+            }
+            : {}
+        }
+      }
+    })
 
     alfy.output(list)
   })
